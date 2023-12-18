ef33fedac43be6941f4a433faae22fab23c10042
AmirNavazi <navazi.amir@gmail.com>
2023-12-18 15:08:03 +0330

    second commit

--- src/PureComponent.jsx
+++ src/PureComponent.jsx
@@ -25,17 +25,19 @@ class PureCompo extends PureComponent{
         return(
             <div className="form-fa-group text-center mt-4 p-3">
 
-                <h4 className="text-center text-dark">حدs کلمه</h4>
+                <h2 className="text-center text-dark">Guess the Word</h2>
 
                 <input ref={this.myInput} type="text" className="form-control" autoComplete="off"/>
 
-                <button className="btn btn-warning my-3" onClick={this.handleChangeName}>ثبت</button>
+                <button className="btn btn-warning my-3" onClick={this.handleChangeName}>Submit</button>
 
-                {/* <button className="btn btn-secondary my-3 mx-2" onClick={()=>{this.myInput.current.value = ""}}>مخفی</button> */}
+                <button className="btn btn-secondary my-3 mx-2" onClick={()=>{this.myInput.current.value = ""}}>Hide</button>
 
                 <br />
 
                 <span>{this.counter++}</span>
+                <br />
+                <h5 className='text-center' style={{marginTop:"5cm",color:"white"}}>Developed by: Amir Hossein Navazi</h5>
 
             </div>
         )
